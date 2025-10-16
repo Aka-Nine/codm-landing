e1951a3b8d4dc57a1e1914a2b56ce48f9309fe4e
harshyadav-web <harsh.yadav@codmsoftware.com>
2025-10-16 13:04:20 +0530

    chage

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,5 +1,6 @@
 "use client";
 import type { PropsWithChildren } from "react";
+import AOS from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
 import Header from "./Header";
@@ -7,7 +8,9 @@ import Footer from "./Footer";
 import BackToTop from "../page_effects/BackToTop";
 
 export default function Layout({ children }: PropsWithChildren) {
-
+  useEffect(() => {
+    AOS.init({ duration: 800, once: true, offset: 80 });
+  }, []);
   return (
     <div className="shell">
       <Header />
